1d9077c406121223bf5e58078a62e174dc97a08f
Hemanathmadusanka <Hemanathmadusanka@gmail.com>
2026-09-20 14:01:17 +0530

    New code

--- main.c
+++ main.c
@@ -2,57 +2,68 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_PATIENTS 100
+
 #define NUM_SPECIALTIES 4
-#define NUM_WARDS 4
-#define MAX_BEDS 20
-const char specialtyName[NUM_SPECIALTIES][30] = {
-    "General Practice (OPD)", "Paediatrics", "Cardiology", "Neurology"
+#define NUM_WARDS       4
+#define BEDS_PER_WARD   20
+#define MAX_PATIENTS    100
+
+
+static const int   specialtyIDs[NUM_SPECIALTIES]     = {1, 2, 3, 4};
+static const char *specialtyNames[NUM_SPECIALTIES]   = {
+    "General Practice (OPD)",
+    "Paediatrics",
+    "Cardiology",
+    "Neurology"
 };
-const float baseFee[NUM_SPECIALTIES]     = {1500.00, 2500.00, 4500.00, 5000.00};
-const int   consultTime[NUM_SPECIALTIES] = {15, 20, 30, 30};
-const int   dailyCap[NUM_SPECIALTIES]    = {30, 20, 12, 10};
+static const float baseFees[NUM_SPECIALTIES]         = {1500.0f, 2500.0f, 4500.0f, 5000.0f};
+static const int   consultTimes[NUM_SPECIALTIES]     = {15, 20, 30, 30};
+static const int   dailyCaps[NUM_SPECIALTIES]        = {30, 20, 12, 10};
 
-const char wardName[NUM_WARDS][30] = {
-    "General Ward", "Paediatric Ward", "Surgical Ward", "ICU (Intensive Care Unit)"
+static const int   wardIDs[NUM_WARDS]                = {1, 2, 3, 4};
+static const char *wardNames[NUM_WARDS]              = {
+    "General Ward",
+    "Pediatric Ward",
+    "ICU",
+    "Neurology Ward"
 };
-const float dailyBedRate[NUM_WARDS] = {3000.00, 6000.00, 12000.00, 25000.00};
-const int   bedCapacity[NUM_WARDS]  = {20, 10, 10, 5};
-
-void showMenu(void);
-
-int main() {
-    int choice;
+static const float wardDailyRates[NUM_WARDS]         = {
+    8000.0f, 10000.0f, 25000.0f, 18000.0f
+};
 
 
-
-    do {
-        showMenu();
-        if (scanf("%d", &choice) != 1) {
-
-            while (getchar() != '\n');
-            choice = -1;
-        }
+static int bedOccupancy[NUM_WARDS][BEDS_PER_WARD];
+static int specialtyQueue[NUM_SPECIALTIES];
 
 
+static int   patientIDs[MAX_PATIENTS];
+static char  patientNames[MAX_PATIENTS][50];
+static int   patientAges[MAX_PATIENTS];
+static int   patientUrgency[MAX_PATIENTS];
+static int   patientSpecialty[MAX_PATIENTS];
+static int   patientAdmitted[MAX_PATIENTS];
+static int   patientWard[MAX_PATIENTS];
+static int   patientBed[MAX_PATIENTS];
+static int   patientDays[MAX_PATIENTS];
+static float patientBaseFee[MAX_PATIENTS];
+static float patientSurcharge[MAX_PATIENTS];
+static float patientWardCost[MAX_PATIENTS];
+static float patientGross[MAX_PATIENTS];
+static float patientDiscount[MAX_PATIENTS];
+static float patientFinal[MAX_PATIENTS];
+static float patientWaitTime[MAX_PATIENTS];
 
-        } while (choice != 5);
-
-    return 0;
+static int patientCount = 0;
 
 
-}
-void showMenu(void) {
-    printf("\n==================================================\n");
-    printf("     SMART HOSPITAL PATIENT & RESOURCE SYSTEM\n");
-    printf("==================================================\n");
-    printf("1. Register New Patient\n");
-    printf("2. View Bed Occupancy Status\n");
-    printf("3. Display Patients by Priority (Triage Order)\n");
-    printf("4. Generate Performance Report\n");
-    printf("5. Exit\n");
-    printf("--------------------------------------------------\n");
-    printf("Enter your choice: ");
-}
+void  printSeparator(void);
+void  printHeader(const char *title);
+void  initializeSystem(void);
+void  showMainMenu(void);
 
 
+int   registerPatient(void);
+void  displayAllPatients(void);
+void  sortByPriority(int order[]);
+void  displayPriorityOrder(void);
+void  generateReports(void);
